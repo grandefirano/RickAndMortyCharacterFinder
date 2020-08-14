ac39b518e4df3bb363f8c9d02439b4785a47edc6
GrandeFirano <jacob.kielar@gmail.com>
2020-08-14 13:56:23 +0200

    with zooming in and out

--- app/src/main/java/com/grandefirano/rickandmortycharacterfinder/list/ListOfCharactersFragment.kt
+++ app/src/main/java/com/grandefirano/rickandmortycharacterfinder/list/ListOfCharactersFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.ScaleGestureDetector
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
@@ -29,13 +30,15 @@ class ListOfCharactersFragment : Fragment() {
 
     private val TAG = "ListOfCharactersFragmen"
 
-    private var searchJob: Job?=null
+    private var searchJob: Job? = null
 
-    val searchRequest:MutableLiveData<Search> =MutableLiveData()
+    val searchRequest: MutableLiveData<Search> = MutableLiveData()
 
     private val adapter by lazy {
         CharactersListAdapter()
     }
+
+    val itemPerRow:MutableLiveData<Int> = MutableLiveData(3)
 
 
     override fun onCreateView(
@@ -43,7 +46,7 @@ class ListOfCharactersFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        val binding=DataBindingUtil.inflate<FragmentListOfCharactersBinding>(
+        val binding = DataBindingUtil.inflate<FragmentListOfCharactersBinding>(
             inflater,
             R.layout.fragment_list_of_characters,
             container,
@@ -51,39 +54,77 @@ class ListOfCharactersFragment : Fragment() {
         )
 
 
+        val manager = GridLayoutManager(context, 3)
+        binding.characterList.adapter = adapter
+        binding.characterList.layoutManager = manager
 
-        val manager=GridLayoutManager(context,3)
-        binding.characterList.adapter=adapter
-        binding.characterList.layoutManager=manager
-
-        binding.lifecycleOwner=this
-
+        binding.lifecycleOwner = this
 
 
-        val searchRes=Search()
-        searchRes.name="rick"
-        searchRes.gender=Search.GenderOption.FEMALE
+        val searchRes = Search()
+        searchRes.name = "rick"
+        searchRes.gender = Search.GenderOption.MALE
 
-        searchRequest.value=searchRes
+        searchRequest.value = searchRes
 
         searchRequest.observe(viewLifecycleOwner, Observer {
-            search(searchRes)
+            search(it)
         })
+
+        itemPerRow.observe(viewLifecycleOwner, Observer {itemPerRow->
+            Log.d(TAG, "onCreateView: itemprerov $itemPerRow")
+            val manager = GridLayoutManager(context, itemPerRow)
+            binding.characterList.adapter = adapter
+            binding.characterList.layoutManager = manager
+        })
+
+        val scaleDetector = ScaleGestureDetector(this.context, PinchListener())
+        binding.characterList.setOnTouchListener { view, motionEvent ->
+            scaleDetector.onTouchEvent(motionEvent)
+            false
+        }
+
 
 
         return binding.root
     }
 
 
-
-    private fun search(query: Search){
+    private fun search(query: Search) {
         searchJob?.cancel()
-        searchJob=lifecycleScope.launch {
+        searchJob = lifecycleScope.launch {
             viewModel.searchCharacters(query).collectLatest {
                 adapter.submitData(it)
             }
         }
+
     }
 
 
+    inner class PinchListener : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+
+        override fun onScaleEnd(detector: ScaleGestureDetector?) {
+            Log.d("TAG", "onScaleEnd:PINCH ")
+
+            detector?.let {
+                Log.d("TAG", "onScaleEnd: ${itemPerRow.value}  ${it.scaleFactor}")
+
+                when {
+                    detector.scaleFactor > 1 -> {
+                        if(itemPerRow.value!! >1) itemPerRow.value=itemPerRow.value?.minus(1)
+                        Log.d("TAG", "onScale: zoom in ${itemPerRow.value}")
+                    }
+                    detector.scaleFactor < 1 -> {
+                        if(itemPerRow.value!!<5)itemPerRow.value=itemPerRow.value?.plus(1)
+                        Log.d("TAG", "onScale: zoom out ${itemPerRow.value}")
+                    }
+                    else -> {
+                        itemPerRow
+                    }
+                }
+            }
+
+        }
+    }
 }
+
