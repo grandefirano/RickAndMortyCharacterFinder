10d917d8e198c6a6931751754be6a52e2356bae1
GrandeFirano <jacob.kielar@gmail.com>
2020-08-15 01:17:20 +0200

    with constraintset

--- app/src/main/java/com/grandefirano/rickandmortycharacterfinder/details/DetailsFragment.kt
+++ app/src/main/java/com/grandefirano/rickandmortycharacterfinder/details/DetailsFragment.kt
@@ -1,14 +1,20 @@
 package com.grandefirano.rickandmortycharacterfinder.details
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import androidx.transition.TransitionManager
 import com.grandefirano.rickandmortycharacterfinder.R
-import com.grandefirano.rickandmortycharacterfinder.databinding.FragmentDetailsBinding
+import com.grandefirano.rickandmortycharacterfinder.databinding.FragmentDetailsBeforeBinding
+import kotlinx.android.synthetic.main.fragment_details_before.*
+
 
 class DetailsFragment : Fragment() {
 
@@ -16,13 +22,14 @@ class DetailsFragment : Fragment() {
 
     private val viewModel: DetailsViewModel by viewModels()
 
+private lateinit var binding:FragmentDetailsBeforeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        val binding=FragmentDetailsBinding.inflate(inflater,container,false)
+        binding=FragmentDetailsBeforeBinding.inflate(inflater,container,false)
 
         binding.lifecycleOwner=viewLifecycleOwner
         val args:DetailsFragmentArgs by navArgs()
@@ -31,7 +38,33 @@ class DetailsFragment : Fragment() {
         binding.character=args.character
 
 
+
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        addAnimationOperations()
+    }
+
+
+    private fun addAnimationOperations(){
+        var set=false
+        val constraint1=ConstraintSet()
+        constraint1.clone(root)
+        val constraint2=ConstraintSet()
+        constraint2.clone(context,R.layout.fragment_details_after)
+
+        binding.portalImageView.setOnClickListener {
+            Log.d("TAG", "addAnimationOperations:CLICK ${Build.VERSION.SDK_INT}")
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                TransitionManager.beginDelayedTransition(root)
+                val constraint = if(set) constraint1 else constraint2
+                constraint.applyTo(root)
+                set = !set
+            }
+        }
+
     }
 
 
